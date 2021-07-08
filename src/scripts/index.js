9d96bfc0eb59e1b2f7e5f1609a3b14c50cec20b6
imamsutono <imamssutono@gmail.com>
2021-07-08 16:37:31 +0700

    Fix code style

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,30 +8,32 @@ const hero = document.querySelector('.hero');
 const main = document.querySelector('main');
 const drawer = document.querySelector('#drawer');
 
-menu.addEventListener('click', function(event) {
+menu.addEventListener('click', (event) => {
   drawer.classList.toggle('open');
   event.stopPropagation();
 });
 
-hero.addEventListener('click', function() {
+hero.addEventListener('click', () => {
   drawer.classList.remove('open');
 });
 
-main.addEventListener('click', function() {
+main.addEventListener('click', () => {
   drawer.classList.remove('open');
 });
 
 function renderRestaurant() {
   let item = '';
 
-  data.restaurants.map(({name, description, pictureId, city, rating}) => {
+  data.restaurants.forEach(({
+    name, description, pictureId, city, rating,
+  }) => {
     item += `<article class="restaurant-item">
         <img class="restaurant-item__thumbnail"
              src="${pictureId}"
              alt="${name}">
         <div class="restaurant-item__content">
             <h1 class="restaurant-item__title"><a href="#">${name}</a></h1>
-            <p class="restaurant-item__description">${description.replace(/^(.{100}[^\s]*).*/, "$1")}</p>
+            <p class="restaurant-item__description">${description.replace(/^(.{100}[^\s]*).*/, '$1')}</p>
             <div class="restaurant-item__info">
               🌟 ${rating}&nbsp;📍 ${city}
             </div>
