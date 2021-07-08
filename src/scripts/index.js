86ce305c4f86f5c9a4a918c43dacdab1d093bf01
imamsutono <imamssutono@gmail.com>
2021-07-08 14:55:54 +0700

    Done submission 1

---
+++ src/scripts/index.js
@@ -0,0 +1,45 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/responsive.css';
+import data from '../DATA.json';
+
+const menu = document.querySelector('#menu');
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function(event) {
+  drawer.classList.toggle('open');
+  event.stopPropagation();
+});
+
+hero.addEventListener('click', function() {
+  drawer.classList.remove('open');
+});
+
+main.addEventListener('click', function() {
+  drawer.classList.remove('open');
+});
+
+function renderRestaurant() {
+  let item = '';
+
+  data.restaurants.map(({name, description, pictureId, city, rating}) => {
+    item += `<article class="restaurant-item">
+        <img class="restaurant-item__thumbnail"
+             src="${pictureId}"
+             alt="${name}">
+        <div class="restaurant-item__content">
+            <h1 class="restaurant-item__title"><a href="#">${name}</a></h1>
+            <p class="restaurant-item__description">${description.replace(/^(.{100}[^\s]*).*/, "$1")}</p>
+            <div class="restaurant-item__info">
+              🌟 ${rating}&nbsp;📍 ${city}
+            </div>
+        </div>
+    </article>`;
+  });
+
+  document.getElementById('restaurantList').innerHTML = item;
+}
+
+renderRestaurant();
