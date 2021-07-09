e15e65c17bc68962304fb3ad190068e3f5750e32
imamsutono <imamssutono@gmail.com>
2021-07-09 10:44:08 +0700

    Add pages and router

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,24 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
+import App from './views/app';
 import data from '../DATA.json';
 
-const menu = document.querySelector('#menu');
+const app = new App({
+  button: document.querySelector('#menu'),
+  drawer: document.querySelector('#drawer'),
+  content: document.querySelector('#maincontent'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
+
+/* const menu = document.querySelector('#menu');
 const hero = document.querySelector('.hero');
 const main = document.querySelector('main');
 const drawer = document.querySelector('#drawer');
@@ -19,7 +34,7 @@ hero.addEventListener('click', () => {
 
 main.addEventListener('click', () => {
   drawer.classList.remove('open');
-});
+}); */
 
 function renderRestaurant() {
   let item = '';
---
+++ src/scripts/views/pages/favorite.js
@@ -0,0 +1,13 @@
+const Detail = {
+  async render() {
+    return `
+      <h2>Favorite Page</h2>
+    `;
+  },
+
+  async afterRender() {
+
+  },
+};
+
+export default Detail;
