5a5c4f39390739eed928c59db5e4e783f836460a
imamsutono <imamssutono@gmail.com>
2021-07-09 08:28:11 +0700

    Fix code style

--- webpack.common.js
+++ webpack.common.js
@@ -13,19 +13,15 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          }
-        ]
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|svg|webp|eot|woff|woff2|ttf)$/,
-        use: ['file-loader']
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
