2962c4593fc863650e29917987df11fb7def5c99
imamsutono <imamssutono@gmail.com>
2021-07-27 21:52:21 +0700

    Change cache name

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://restaurant-api.dicoding.dev/';
 const CONFIG = {
   BASE_URL,
   BASE_IMAGE_URL: `${BASE_URL}/images/`,
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'luwee',
   DATABASE_NAME: 'luwee-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
