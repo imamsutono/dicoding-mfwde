dfacbf546bd57fca7295dc76b036d46e7f9c6df6
imamsutono <imamssutono@gmail.com>
2021-07-27 22:18:19 +0700

    Fix bug integration test

--- specs/helpers/testFactories.js
+++ specs/helpers/testFactories.js
@@ -1,8 +1,10 @@
 import LikeButtonPresenter from '../../src/scripts/utils/like-button-presenter';
+import FavoriteIdb from '../../src/scripts/data/favorite-idb';
 
 const createLikeButtonPresenterWithRestaurant = async (restaurant) => {
   await LikeButtonPresenter.init({
     likeButtonContainer: document.querySelector('#likeButtonContainer'),
+    favorite: FavoriteIdb,
     restaurant,
   });
 };
